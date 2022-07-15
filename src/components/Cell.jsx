668bd463237354cb0b98cf294484ed2aad373c82
Iskait <88732417+Iskait@users.noreply.github.com>
2022-07-15 22:54:12 +0300

    some fixes, add show correct answer

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,31 +1,42 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { restartGame } from '../store/slices/labyrinthSlice';
-import thumbUp from '../assets/thumb-up.svg';
-import thumbDown from '../assets/thumb-down.svg';
-function Cell({picked, start}) {
-  const { path } = useSelector(state=>state.labyrinth);
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { restartGame } from "../store/slices/labyrinthSlice";
+import { setIsRight, setShowAnswer } from "../store/slices/answerSlice";
+import thumbUp from "../assets/thumb-up.svg";
+import thumbDown from "../assets/thumb-down.svg";
+
+function Cell({ picked, start }) {
+  const { path } = useSelector((state) => state.labyrinth);
+  const { isRight, showAnswer } = useSelector(state=>state.answer);
   const dispatch = useDispatch();
   const [thumbVisible, setThumbVisible] = useState(false);
   const handleGuess = () => {
     if (path.length !== 10) return;
     setThumbVisible(true);
-    setTimeout(() => {dispatch(restartGame()); setThumbVisible(false)}, 2000);
-  }
+    dispatch(setIsRight(picked));
+    dispatch(setShowAnswer(true));
+    setTimeout(() => {
+      dispatch(restartGame());
+      setThumbVisible(false);
+      dispatch(setIsRight(true));
+      dispatch(setShowAnswer(false));
+    }, 2000);
+  };
   return (
-    <div 
-    className="cell"
-    onClick={handleGuess}>
-        {start && 
-        <p className="cell__start">START</p>
-        }
-        {thumbVisible && 
-        <img 
-        src={picked ? thumbUp : thumbDown} 
-        style={{width: '20px', height: '20px'}}
-        alt="thumb-up"/>}
+    <div className="cell" onClick={handleGuess}>
+      {start && <p className="cell__start">START</p>}
+      {showAnswer && !isRight && picked && (
+        <span className="cell__answer">*</span>
+      )}
+      {thumbVisible && (
+        <img
+          src={picked ? thumbUp : thumbDown}
+          style={{ width: "20px", height: "20px" }}
+          alt="thumb-up"
+        />
+      )}
     </div>
-  )
+  );
 }
 
-export default Cell
+export default Cell;
--- src/components/Labyrinth.jsx
+++ src/components/Labyrinth.jsx
@@ -1,9 +1,13 @@
 import Cell from "./Cell";
-import { useSelector, useDispatch } from 'react-redux';
-import { getCoordinates, switchCells, setStart } from '../store/slices/labyrinthSlice';
+import { useSelector, useDispatch } from "react-redux";
+import {
+  getCoordinates,
+  switchCells,
+  setStart,
+} from "../store/slices/labyrinthSlice";
 import { useEffect } from "react";
 function Labyrinth() {
-  const {coordinate, path, field} = useSelector(state => state.labyrinth);
+  const { coordinate, path, field } = useSelector((state) => state.labyrinth);
   const dispatch = useDispatch();
   useEffect(() => {
     let timeoutId = null;
@@ -14,28 +18,32 @@ function Labyrinth() {
       timeoutId = setTimeout(() => {
         dispatch(getCoordinates());
         dispatch(switchCells(coordinate));
-      }, 1000)
-      return () => {
-        clearTimeout(timeoutId);
-      }
+      }, 1000);
     }
-  }, [coordinate, path, dispatch])
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [coordinate, path, dispatch]);
   return (
     <div className="labyrinth">
       <div className="labyrinth__container _container">
         <h1 className="labyrinth__title">Labyrinth game</h1>
-        <p 
-        className="labyrinth__text"
-        style={{visibility: path.length === 10 ? 'visible' : 'hidden'}}>Try to guess.
+        <p
+          className="labyrinth__text"
+          style={{ visibility: path.length === 10 ? "visible" : "hidden" }}
+        >
+          Try to guess.
         </p>
         <div className="labyrinth__block">
-          {field.map((cell, idx) => (
-            <div 
-            className="labyrinth__column"
-            key={idx}>
-              {cell.map(x=> <Cell key={x.cellNo} picked={x.picked} start={x.start}/>)}
-            </div>
-          ))}
+          {field.map((cell) =>
+            cell.map((x) => (
+              <Cell
+                key={x.cellNo}
+                picked={x.picked}
+                start={x.start}
+              />
+            ))
+          )}
         </div>
       </div>
     </div>
