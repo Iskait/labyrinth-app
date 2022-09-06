d597ad3748840574936427ac277eaeecd8312b4d
Maxim Kuzin <iskait04@gmail.com>
2022-09-06 15:45:49 +0300

    Стилизация проекта tailwind #1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './styles/all.scss'
-import Labyrinth from './components/Labyrinth';
-import Path from './components/Path';
+import "./index.css";
+import Labyrinth from "./components/Labyrinth";
+import Path from "./components/Path";
 function App() {
   return (
-    <div className="wrapper">
-      <Labyrinth />
-      <Path />
+    <div className="w-screen h-screen bg-[url('./assets/background.jpeg')] bg-cover bg-no-repeat flex flex-col font-serif">
+      <div className="container mx-auto">
+        <Labyrinth />
+        <Path />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
